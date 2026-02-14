c22e553fd4bdbe037fb92187428ec2667856bea9
DivyamJ-07 <jaindivyam1968@gmail.com>
2026-02-14 15:53:21 +0530

    Add circle area calculation in 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    double r;
+    std::cout<<"Enter the radius of the circle : ";
+    std::cin>>r;
+    std::cout<<"The radius of the fountain of the circle is : "<<3.14*r*r;
+    return 0;
+}
